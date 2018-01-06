393ead8c88a1354a7891de29f374f04a5fe22a2b
Fendo181 <fendo181git@gmail.com>
2018-01-06 22:36:11 +0900

    add hello.php
    p32

---
+++ chap2/hello.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>PHPの基本構文</title>
+    </head>
+    <body>
+    <?php
+        print 'Hello wold!';
+        print 'My PHP Study Start';
+    ?>
+    </body>
+</html>
