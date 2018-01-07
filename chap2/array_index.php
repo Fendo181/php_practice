d8f239a372f733b2c91a8e9a1e67b8cc5c7ace5e
Fendo181 <fendo181git@gmail.com>
2018-01-07 11:23:14 +0900

    add array_index.php
    p68
    通常配列と連想配列の関係
    本質的には連想配列しかない。つまり区別されてない。
    もし連想配列でインデックスを省略した場合「その時点での最大インデックス+1」が自動的に振られる。

---
+++ chap2/array_index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/01/07
+ * Time: 11:15
+ */
+
+$date = [1 => 'endo', 2 =>'tanaka', 3 =>'mitani'];
+print_r($date);
+print_r($date[1]);
+
+$date2 = [1 => 'apple', 4 => 'banana', 'endo'];
+print_r($date2);
+
+
+//(
+//[1] => apple
+//[4] => banana
+//[5] => endo
+//)
