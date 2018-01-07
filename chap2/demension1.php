b428542a6593584a369c281b48806c6143eb7c2e
Fendo181 <fendo181git@gmail.com>
2018-01-07 11:09:04 +0900

    add deension1.php
    p65
    多次元配列

---
+++ chap2/demension1.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/01/07
+ * Time: 11:04
+ */
+
+$date1 = [
+    ['X-1', 'X-2', 'X-3'],
+    ['Y-1', 'Y-2', 'Y-3'],
+    ['Z-1', 'Z-2', 'Z-3'],
+];
+
+print_r($date1);
+print_r($date1[0][0]);
+print_r($date1[1][2]);
---
+++ chap2/demension3.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/01/07
+ * Time: 11:09
+ */
