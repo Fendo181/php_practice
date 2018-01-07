15ec70351f1750fe29a606a8b9fff92963cbb200
Fendo181 <fendo181git@gmail.com>
2018-01-07 10:55:06 +0900

    add array.php
    p62
    配列
    [](ブラケット構文)

---
+++ chap2/array.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/01/07
+ * Time: 10:51
+ */
+
+$data = ['endo','takahasi','nomoto'];
+print $data; // Array
+var_dump($data);
+
+print $data[2];
