26a1d92201251b3524d4b717a69a9ca5675a95ef
Fendo181 <fendo181git@gmail.com>
2018-01-07 11:04:04 +0900

    add assoc.php
    p65
    連想配列
    
    [
     'key' => 'value'
    ];

---
+++ chap2/assoc.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/01/07
+ * Time: 11:01
+ */
+
+$members = [
+    'apple' => 'red',
+    'sea'   => 'blue',
+    'mikan' => 'orange',
+];
+
+print_r($members);
+print  ($members['apple']);
