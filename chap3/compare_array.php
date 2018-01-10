48d1c1907c46c677e0c7220be76a26c0e7b89eff
Fendo181 <fendo181git@gmail.com>
2018-01-11 07:33:23 +0900

    p92 add compare_array.php
    配列同士を比較を行う

--- chap3/compare_array.php
+++ chap3/compare_array.php
@@ -5,3 +5,23 @@
  * Date: 2018/01/11
  * Time: 7:23
  */
+
+$data1 = [1,2,];
+$data2 = [1,2,3];
+
+//要素数の数で比較
+var_dump($data1 < $data2);
+
+//要素数が同じ場合
+
+$data11 = [1,2,4];
+$data21 = [1,2,3];
+
+//要素数が等しい場合は同じキーをもつ要素通しで比較を行う。
+var_dump($data11 < $data2);
+
+$data12 = [1,2,3];
+$data22 = [1,2,'3'];
+
+var_dump($data12 == $data22); #false
+var_dump($data12 === $data22); #true
--- chap3/eqal.php
+++ chap3/eqal.php
@@ -5,3 +5,7 @@
  * Date: 2018/01/10
  * Time: 8:52
  */
+
+var_dump('13xyz' == 13); #true
+var_dump('13xyz' === 13); #false
+var_dump('13xyz' == '13'); #true
