43af7fd94bd01bd9e38c3971507e9a0cc4ac2f82
Fendo181 <fendo181git@gmail.com>
2018-01-12 08:25:52 +0900

    p96 add compair_2.php
    正しく比較を行う場合は===まで使ったほうがい

---
+++ chap3/compare_array2.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/01/12
+ * Time: 8:23
+ */
+
+var_dump('1.44E2' === 144); //false
+var_dump('1.44E2' === 144); //false
+var_dump('0x10' === '16'); //false
+var_dump('H' === 0); //false
