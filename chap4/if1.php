3533fa62bab76f9b974c805b119cb75495c42020
Fendo181 <fendo181git@gmail.com>
2018-01-13 08:51:17 +0900

    p111 add if1.php
    条件分岐

---
+++ chap4/if1.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/01/13
+ * Time: 8:48
+ */
+
+$x = 10;
+
+if($x === 10){
+    print "This is 10";
+} else {
+    print "THIS is not 10";
+}
