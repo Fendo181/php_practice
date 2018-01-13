09aaa5c757ed64ca5acc9edcb6ab4069f5d34f4b
Fendo181 <fendo181git@gmail.com>
2018-01-13 09:35:25 +0900

    p112 ad elseif.php

---
+++ chap4/ifelse.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/01/13
+ * Time: 8:55
+ */
+
+$x = 10;
+
+if ($x >5){
+    print "High than 5!";
+} elseif ($x === 5) {
+    print "equal 5";
+} else {
+    print "Low than 5";
+}
