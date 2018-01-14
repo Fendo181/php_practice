1a8b8d091a5bc9ceb507016237aaad532f9a1a78
Fendo181 <fendo181git@gmail.com>
2018-01-14 23:46:37 +0900

    p113 update nest.php
    `===`で厳密に値を比較するようにした

--- chap4/nest.php
+++ chap4/nest.php
@@ -9,10 +9,10 @@
 $x = 1;
 $y = 2;
 
-if ($x == 1){
-    if($y == 1) {
+if ($x === 1){
+    if($y === 1) {
         print "x=1,y=1";
-    }else if($y ==2) {
+    }else if($y === 2) {
         print "x=1,y=2";
     }
 }
