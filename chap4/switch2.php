24c871e6aaa09390626033e8933c77489d219175
Fendo181 <fendo181git@gmail.com>
2018-01-15 08:41:14 +0900

    p118 add switch2.php
    switch文によるフォークスルー

---
+++ chap4/switch2.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/01/15
+ * Time: 8:36
+ */
+
+//フォークスルー
+
+$drink = 'ブランデ';
+
+switch ($drink){
+    case 'ビール':
+    case 'ワイン':
+    case '日本酒':
+        print '醸造酒です';
+        break;
+    case 'ブランデ':
+    case 'ウィスキー':
+        print '蒸留酒です';
+        break;
+}
