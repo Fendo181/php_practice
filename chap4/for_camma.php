d7cd464ab27f0f0a314f7c17cd987643e0bd496a
Fendo181 <fendo181git@gmail.com>
2018-01-17 08:32:34 +0900

    p123 add for_comma.php
    カンマ演算子はコードをコンパクトにすることができる
    が、濫用すべきではない

---
+++ chap4/for_camma.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/01/17
+ * Time: 8:28
+ */
+
+for ($i = 1, $j=1; $result = $i * $j, $i < 6; $i++,$j++){
+    print "{$i}×{$j}={$result} nl";
+}
