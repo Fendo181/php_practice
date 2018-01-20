9d606d391b82fdb37d09982db382e32e93ec4acd
Fendo181 <fendo181git@gmail.com>
2018-01-20 22:20:07 +0900

    p128 add foreach_asscoc.php
    foreach文を使って連想配列をすべて出力してみる。
    通常配列の場合はindexがkeyに相当する。

---
+++ chap4/foreach_assoc.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/01/20
+ * Time: 22:15
+ */
+
+$data = [ 'endo' => '23',
+          'takahashi' => '27',
+          'mitani' => '56',
+          'nomoto' => '23'
+        ];
+
+foreach($data as $key => $value){
+    print "{$key}の年齢は{$value}です";
+}
