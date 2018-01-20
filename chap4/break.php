762ceaef8468e679733780737ae164344735c584
Fendo181 <fendo181git@gmail.com>
2018-01-20 22:45:09 +0900

    p131 add break.php

---
+++ chap4/break.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/01/20
+ * Time: 22:42
+ */
+
+$sum = 0;
+for($i = 1; $i <= 100; $i++){
+    $sum += $i;
+    print $sum;
+    if ($sum > 1000){ break; }
+}
+
+print  "合計は1000を超えるときは1~${i}を加算したときです";
