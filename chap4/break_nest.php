73573ca1e77fdcc839c2621b6871dd0eacab2806
Fendo181 <fendo181git@gmail.com>
2018-01-20 23:05:10 +0900

    p132 add break_nest.php

---
+++ chap4/break_nest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/01/20
+ * Time: 22:52
+ */
+
+for($i=1; $i <10; $i++){
+    for($j =1; $i < 10; $j++){
+        $result = $i * $j."\t";;
+        if($result > 40){ break; }
+        print "{$result}";
+    }
+    echo PHP_EOL;
+}
