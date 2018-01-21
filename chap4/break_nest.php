3933a983c15f9955583c13c9cca16c3b314c1f00
Fendo181 <fendo181git@gmail.com>
2018-01-22 08:03:15 +0900

    p132 update break_nest.php
    break ループ階層数
    を指定することで、内側、外側の2重ループからまとめて脱出します

--- chap4/break_nest.php
+++ chap4/break_nest.php
@@ -9,7 +9,7 @@
 for($i=1; $i <10; $i++){
     for($j =1; $i < 10; $j++){
         $result = $i * $j."\t";;
-        if($result > 40){ break; }
+        if($result > 40){ break 2; }
         print "{$result}";
     }
     echo PHP_EOL;
