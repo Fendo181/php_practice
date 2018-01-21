86aee9038d52a124f77010890ea7e93f9c290542
Fendo181 <fendo181git@gmail.com>
2018-01-22 08:12:59 +0900

    p139 add foreach_sample.php
    参照渡しによるforeach文

---
+++ chap4/foreach_sample.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/01/22
+ * Time: 8:09
+ */
+
+$data = [10,25,50];
+
+foreach ($data as &$item){
+    $item = $item * 1.5;
+}
+print_r($data);
