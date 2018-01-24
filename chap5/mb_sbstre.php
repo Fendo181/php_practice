c64d66b6a8aa690ae3073ba9cd15b6080f912938
Fendo181 <fendo181git@gmail.com>
2018-01-24 22:52:13 +0900

    p148 add mb_substr.php

--- chap5/mb_sbstre.php
+++ chap5/mb_sbstre.php
@@ -5,3 +5,15 @@
  * Date: 2018/01/24
  * Time: 22:37
  */
+
+mb_internal_encoding('utf-8');
+$str = "EndoTechBlog";
+
+// 0文字目から4文字分
+print mb_substr($str, 0, 4);
+echo PHP_EOL;
+// ５文字目以降
+print mb_substr($str, 4);
+echo PHP_EOL;
+//0文字目以降末にから4文字カットした。
+print mb_substr($str, 0, -4);
