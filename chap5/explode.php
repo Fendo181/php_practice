5e63f870f6c288ec324d22a0a11dae9c7a63f379
Fendo181 <fendo181git@gmail.com>
2018-01-26 08:24:04 +0900

    p152 add explode.php
    文字列を特定の区切り文字で分別する。

---
+++ chap5/explode.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/01/26
+ * Time: 8:19
+ */
+
+$data = 'リオとニンザブロウとリンリン';
+//とで区切る。
+print_r(explode('と',$data));
+// なし
+print_r(explode('や',$data));
+// 2要素でいい感じに区切る。
+print_r(explode('と',$data,2));
+// 最後から2つ要素目までは切る
+print_r(explode('と',$data,-2));
