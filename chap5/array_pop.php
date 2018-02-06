e950c5513d3cd2c09ba2565287eebb87c6d60615
Fendo181 <fendo181git@gmail.com>
2018-02-07 08:20:27 +0900

    p168　update array_pop.php
    pushは末尾に追加
    popは末尾から取得
    shiftは先頭要素から値を抜く。
    unshiftは先頭要素に値を追加する。

--- chap5/array_pop.php
+++ chap5/array_pop.php
@@ -9,10 +9,24 @@
 $data = ['高江','青木','土井'];
 # 末尾に追加
 array_push($data,'遠藤', '高橋');
+echo PHP_EOL;
 print_r($data);
 
 # 末尾の値を抜く・この場合は高橋がぬける。
 array_pop($data);
+echo PHP_EOL;
 print_r($data);
+
+//先頭から値を抜く。 '高江が抜かれている。'
+print array_shift($data);
+echo PHP_EOL;
+print_r($data);
+
+//先頭から値を抜く。
+print array_unshift($data,'endo','takahashi');
+echo PHP_EOL;
+print_r($data);
+
+
 
 
