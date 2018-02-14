bdcc412c8a2ca44547a27febbbdd8b9a1abefcff
Fendo181 <fendo181git@gmail.com>
2018-02-15 07:03:55 +0900

    add   p118 function_traiangle_type.php

---
+++ chap6/function_triangle_type.php
@@ -0,0 +1,16 @@
+<?php
+// ユーザ定義関数の第一歩は重複をなくすこと
+// $base = 8;
+// $hight = 10;
+// $area = $base * $hight;
+// print "三角形の面積は{$area}です";
+
+function getTriangleArea(float $base,float $hight): float {
+    return $base * $hight/2;
+}
+
+$area = getTriangleArea(8,'1');
+print "三角形の面積は{$area}です";
+
+// 型は存在するが
+// $area = getTriangleArea(8,'1');は通ってしまう。
