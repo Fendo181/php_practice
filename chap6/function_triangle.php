8c163ccd0fa256e79848a133ab80baad7fa48beb
Fendo181 <fendo181git@gmail.com>
2018-02-15 06:51:05 +0900

    p216 add function_triangle.php
    ユーザ定義関数を設定して三角形の面積を求めるfunctionを作成した。

---
+++ chap6/function_triangle.php
@@ -0,0 +1,13 @@
+<?php
+// ユーザ定義関数の第一歩は重複をなくすこと
+// $base = 8;
+// $hight = 10;
+// $area = $base * $hight;
+// print "三角形の面積は{$area}です";
+
+function getTriangleArea($base,$hight){
+    return $base * $hight;
+}
+
+$area = getTriangleArea(8,10);
+print "三角形の面積は{$area}です";
