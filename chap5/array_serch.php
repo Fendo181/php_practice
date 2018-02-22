a154f0ffbde1a88c9de19de93ab7cba525a8feba
Fendo181 <fendo181git@gmail.com>
2018-02-22 22:35:36 +0900

    p174 update array_serch.php

--- chap5/array_serch.php
+++ chap5/array_serch.php
@@ -1,7 +1,12 @@
 <?php
 
-$data = ['endo', 'takahashi', 'mitani'];
+$data = ['endo', 'takahashi', 'mitani','15'];
 var_dump(array_search('endo', $data)); #0
 var_dump(array_search('mitani', $data)); #2
 var_dump(array_search('yokota', $data)); #false
+
+var_dump(array_search(15, $data)); #数値と文字列比較は `==`なのでint(3)
+var_dump(array_search(15, $data, true)); #false 第三引数にtrueを渡すと現道にチェックをする。
+
+
 
