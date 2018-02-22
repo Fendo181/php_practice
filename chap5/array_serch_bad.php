65d5477ec0470d957eaaf461bb6b13063797ac45
Fendo181 <fendo181git@gmail.com>
2018-02-22 22:41:27 +0900

    p175 update array_serch_bad.php
    in_arrayを使ってちゃんとtrueかfalseを使って判定を行うようにした。

--- chap5/array_serch_bad.php
+++ chap5/array_serch_bad.php
@@ -2,6 +2,6 @@
 
 $data = ['PHP', ''=>'JavaScript', 'PHP', 'Java', 'C#', '15'];
 var_dump(array_search('PHP', $data)); #int(0)
-if(!array_search('PHP',$data)){
+if(!in_array('Ruby',$data)){
     print '見つかりませんでした！';
 }
