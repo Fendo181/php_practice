04cc6fae3f6d15011be54f16cc96d4c8b8b57f90
Fendo181 <fendo181git@gmail.com>
2018-03-16 09:17:52 +0900

    p178 add usort.php
    keyに記述した法則に従って、dataがsortされる。

---
+++ chap5/usort.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/03/16
+ * Time: 9:07
+ */
+
+//bool usort ( array &$array , callable $value_compare_func )
+
+$keys = ['banana', 'apple', 'orange'];
+$data = ['apple','banana','orange'];
+
+usort($data, function ($a, $b) use ($keys) {
+    return array_search($a, $keys) <=> array_search($b,$keys);
+});
+
+print_r($data);
