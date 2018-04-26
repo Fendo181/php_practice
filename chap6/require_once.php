25795d80af36a3515dc36b1acfac68cc96bb6fc7
Fendo181 <fendo181git@gmail.com>
2018-04-27 07:11:56 +0900

    add p22 require_once.php include.php
    基本的には`require_once`で良い。

---
+++ chap6/require_once.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/04/27
+ * Time: 7:07
+ */
+
+require_once  'include.php';
+$area = getTriangleArea(8, 10);
+print "三角形の面積は{$area}";
