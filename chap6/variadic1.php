cd9b6acd8685bd97d25c445c23033ca9954cf5ba
Fendo181 <fendo181git@gmail.com>
2018-05-18 08:17:18 +0900

    p234 可変長引数の関数 varidic1.php
    function(... 仮引数)
    で渡ってきた仮引数分の値が取得できる。

---
+++ chap6/variadic1.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/05/18
+ * Time: 8:10
+ */
+
+// ...で
+function sum(float ...$args): float{
+    // 結果を格納する為の変数 $result
+    $result = 0;
+    // 取得した引数を額に加算処理
+    foreach ($args as $arg){
+        $result += $arg;
+    }
+    return $result;
+}
+
+print sum(1,2,3,4,5,6,7,8,9,10);
