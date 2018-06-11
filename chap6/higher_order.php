0eba3f98e967a2e8ff8ab5d30f39932796eb0817
Fendo181 <fendo181git@gmail.com>
2018-06-12 08:27:39 +0900

    p242 update 高階関数
    第二引数にメソッド名を指定してあげると、callbackで関数を呼びだす事ができる

--- chap6/higher_order.php
+++ chap6/higher_order.php
@@ -7,7 +7,16 @@
  */
 
 // 高階関数
+// 関数内から
 
 function my_array_walk(array $array, callable $func){
-
+    foreach ($array as $key => $value) {
+        $func($value, $key);
+    }
 }
+function showItem($value, $key) {
+    print "{$key}:{$value}";
+}
+
+$data = ['endo', 'natumi', 'takahashi'];
+my_array_walk($data, 'showItem');
