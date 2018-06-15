1b59f6735b7cca53c75e6b204212e0a978b2ad0c
Fendo181 <fendo181git@gmail.com>
2018-06-16 07:22:58 +0900

    p248 use:親スコープ
    useを使う事で親スコープの変数を取得する事ができる。
    従って関数内で変数を関数に引き渡す際にはuseを呼び出して使うと良い。

--- chap6/closure_use2.php
+++ chap6/closure_use2.php
@@ -6,18 +6,24 @@
  * Time: 8:37
  */
 
-function hoge() {
+function hoge()
+{
     $data = [100, 200, 300, 400];
     # ローカルスコープ
     $result = 0;
 
-    my_array_walk($data ,function (float $value, int $key) {
+    function my_array_walker(array $array, callable $func){
+        foreach ($array as $key => $value) {
+            $func($value, $key);
+        }
+    }
+
+    my_array_walker($data, function (float $value, int $key) use(&$result){
         // globalは関数の外側の変数を取得しにいくので結局は値は0になる
-        global $result;
         $result += $value;
     });
 
-    print "{result}";
+    print "{$result}";
 }
 
 hoge();
