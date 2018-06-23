80cf6bb7ff1307cb2570bf5f0de677b273169d43
Fendo181 <fendo181git@gmail.com>
2018-06-23 23:29:34 +0900

    p421 最も簡単なクラス
    basic.phpを使ってクラスをインスタンス化させて、オブジェクトを生成した

---
+++ chap10/basic.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/06/23
+ * Time: 23:26
+ */
+
+require_once ('Person1.php');
+
+$person = new Person();
+var_dump($person);
+
+//object(Person)#1 (0) {
+//}
