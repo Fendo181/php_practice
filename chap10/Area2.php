5de7ddc2c2f83e2e7e8f4352c40e08141bc479a7
Fendo181 <fendo181git@gmail.com>
2018-06-28 08:19:15 +0900

    p428 静的プロパティ
    オブジェクトを経由せずに呼び出せプロパティが静的プロパティです
    原則として、静的プロパティを定義する際は読み取り専用にすべきです

---
+++ chap10/Area2.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/06/28
+ * Time: 8:14
+ */
+
+
+class Area
+{
+    public static $pi = 3.14;
+
+    public static function circle(float $redius): float {
+        return pow($redius,2) * self::$pi;
+    }
+}
+
