a359f7ec36c64589575f083d3ed63de2b5d15e95
Fendo181 <fendo181git@gmail.com>
2018-07-01 14:15:18 +0900

    p444 スーパクラスのコンストラクタを呼び出して、オーバライドする
    parent::__constructoreで親クラスのコンストラクタを呼び出す

---
+++ chap10/override3.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/07/01
+ * Time: 14:02
+ */
+
+
+require_once 'Foreigner.php';
+
+$f = new Foreigner('菊池','通称"kikuchi1201','翔太');
+$f->show();
