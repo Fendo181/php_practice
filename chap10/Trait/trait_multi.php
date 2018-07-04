a94f5e0611eb4a085c1918c16867c46bc0904863
Fendo181 <fendo181git@gmail.com>
2018-07-05 08:47:11 +0900

    p460 トレイトと多重継承
    インターフェースが型
    トレイトが実装
    
    抽象メソッドは単一継承でかつ、オーバライドさせることを前提とした型
    と言ってもいいのかな

---
+++ chap10/Trait/trait_multi.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/07/05
+ * Time: 8:40
+ */
+
+//型を宣言する
+
+interface IFax {
+    function send();
+}
+
+interface IPrinter {
+    function print();
+}
+
+// 実装を宣言する
+trait FaxTrait {
+    public function send()
+    {
+        print 'sending fax...sended!';
+        echo PHP_EOL;
+    }
+}
+
+trait PrinterTrait {
+    public function print() {
+        print 'printing completed';
+        echo PHP_EOL;
+    }
+}
+
+
+// interfaceが型(そのメソッドを実装ないと駄目だよ〜)
+// traitが実装(再利用可能な実装をするよ~)
+
+class FaxPrinter implements IFax, IPrinter {
+    use FaxTrait, PrinterTrait;
+}
+
+
+$fp = new FaxPrinter();
+$fp->send();
+$fp->print();
