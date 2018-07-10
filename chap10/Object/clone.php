693166524394de2898419dfb37018a10a6517add
Fendo181 <fendo181git@gmail.com>
2018-07-10 23:25:34 +0900

    p468 オブジェクトの代入
    オブジェクト変数だけは例外的に参照渡しがデフォルトです

---
+++ chap10/Object/clone.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/07/10
+ * Time: 23:22
+ */
+
+require_once 'Person.php';
+
+$p1 = new Person('リオ','山田');
+$p2 = $p1;
+$p2->firstName = '直美';
+$p2->lastName = '掛谷';
+
+print_r($p1);
+print_r($p2);
