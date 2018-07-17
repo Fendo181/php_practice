8d4c178c63005786f26fdda07fe2628da2a2988b
Fendo181 <fendo181git@gmail.com>
2018-07-18 08:46:51 +0900

    p474 例外処理
    throw 例外オブジェクトを投げる
    ExceptionクラスはPHPが標準で提供している例外クラスでもあり、全ての例外クラスのスーパクラスでもある
    つまりすべての例外クラスは、Exceptionクラスを継承しているという事です。

---
+++ chap10/Exception/throw1.php
@@ -0,0 +1,25 @@
+<?php
+
+class MyUtil {
+    public static function getContents(string $url): string {
+        // 指定されたURLの形が正しくない場合...
+        if(!preg_match('|http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?)|',$url)) {
+            throw new  Exception('不正なURLの形式です！');
+        }
+
+        $data = @file_get_contents($url);
+
+        //データを取得できなかった場合
+        if(!$data) {
+            throw new Exception('指定されたURLが見つかりません。');
+        }
+        return $data;
+    }
+}
+
+//例外処理
+try {
+    print MyUtil::getContents('http://wwww.wings.msn.to/nothing/');
+} catch (Exception $exception) {
+    print "エラーメッセージ: {$exception->getMessage()}";
+};
