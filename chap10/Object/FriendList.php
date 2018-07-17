5831075f6f2e334919e35e7d65413824180f1cb4
Fendo181 <fendo181git@gmail.com>
2018-07-17 22:24:54 +0900

    p472 オブジェクトの反復処理のカスタマイズ

---
+++ chap10/Object/FriendList.php
@@ -0,0 +1,27 @@
+<?php
+
+//IteratorAggregateはPHPが標準でもっている韻t−フェースです
+
+class FriendList implements IteratorAggregate
+{
+    // ダミープロパティ
+    public $version = '1.0.0';
+    public $name = '';
+    // Personオブジェクトのリストを格納するためのprivate変数
+    private $list = [];
+
+    // getIteratorをオーバライドする。
+    // 列にイテレータとしての機能を提供します
+    // Traversableそのクラスの中身が foreach を使用してたどれるかどうかを検出するインターフェイスです。
+    public function getIterator(): Traversable
+    {
+        return new ArrayIterator($this->list);
+    }
+
+
+    //Personオブジェクトを追加する為のメソッド
+    public function add(Person $p)
+    {
+        $this->list[] = $p;
+    }
+}
