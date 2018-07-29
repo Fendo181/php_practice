ca67773120b33892c3b6d51ff11281a5ef6863ca
Fendo181 <fendo181git@gmail.com>
2018-07-30 08:36:59 +0900

    p488 マジックメソッド __call
    コードを一部修正

--- chap10/MagicMethod/Animal.php
+++ chap10/MagicMethod/Animal.php
@@ -16,13 +16,11 @@ class Animal
      */
     public function __call($name, $arguments)
     {
-        var_dump($arguments);
         // 引数が渡されなかった場合はメソッド名に対応するキーの値を取得する。
         if(count($arguments) === 0) {
             return $this->animals[$name];
-        // 引数が渡されなかった場合は、メソッド名に対応するキーの引数の値を設定する。
+        // 引数が渡された場合は、メソッド名に対応するキーの引数の値を設定する。
         } else {
-            var_dump($arguments[0]);
             return $this->animals[$name] = $arguments[0];
         }
     }
