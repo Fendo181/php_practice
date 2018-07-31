5aaebcc78ca175e2e5f060ae7d5aa12152cf68fa
Fendo181 <fendo181git@gmail.com>
2018-08-01 07:50:48 +0900

    p492 オブジェクトの複製方法をカスタマイズする
    この場合はクローンしたオブジェクトが参照渡しがされるだけであるので、シャローコピー

---
+++ chap10/MagicMethod/clone_deep.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/08/01
+ * Time: 7:40
+ */
+
+require_once 'Person.php';
+require_once 'FriendList.php';
+
+$list = new FriendList();
+$list->add(new Person('太郎','山田'));
+$list->add(new Person('遠藤','太徳'));
+$list->add(new Person('高橋','清太郎'));
+
+$list2 = clone $list;
+// シャローコピー 参照はそのままのコピーになる。
+var_dump($list2(1) === $list(1)); #true
