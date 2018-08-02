f0ba4c02b1a4d83e4e98d65afedcfcb50e08e915
Fendo181 <fendo181git@gmail.com>
2018-08-02 10:17:23 +0900

    p499 名前の記法:修飾名
    この場合は名前は現在の名前空間を基準に解釈されます。
    この場合は`qualifier.php`がchap10に所属しているので、hoge\MyClassは
    chap10\hoge\MyClaasとして認識される

---
+++ chap10/NameSpace/qualifier.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/08/02
+ * Time: 10:04
+ */
+
+namespace chap10;
+
+require_once 'hoge/MyClass.php';
+
+print hoge\MyClass::showClass();
