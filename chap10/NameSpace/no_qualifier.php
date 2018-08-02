800c2e8b6501da8517d2c2045272eaf93bcbd986
Fendo181 <fendo181git@gmail.com>
2018-08-02 10:04:19 +0900

    p498 名前の記法:非修飾名

---
+++ chap10/NameSpace/no_qualifier.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/08/02
+ * Time: 10:00
+ */
+
+namespace chap10\hoge;
+
+// 非修飾
+// グローバル名前空間に属するPersonクラスを表している。
+
+
+require_once 'MyClass.php';
+print MyClass::showClass(); ## chap10\hoge\MyClass%
+
