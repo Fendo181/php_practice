2bcbc8a46c0d70cad9d77f9df399bea1614095ae
Fendo181 <fendo181git@gmail.com>
2018-08-03 09:01:09 +0900

    p502 名前競合時に別名を設定する
    as Hoge でエイリアスを設定する事ができる

---
+++ chap10/NameScope/use_ailias.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/08/03
+ * Time: 8:57
+ */
+
+require_once 'MyClass.php';
+require_once 'hoge/MyClass.php';
+
+use chap10\NameScope\MyClass;
+use chap10\NameScope\hoge\MyClass as SubClass;
+
+
+print MyClass::showClass(); #chap10\NameScope\MyClass
+print SubClass::showClass(); #MyClass1chap10\NameScope\hoge\MyClass%
+
+
+
