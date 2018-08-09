8ca1d0d4f828fe7bd2b4bf338c61df38706c7ca7
Fendo181 <fendo181git@gmail.com>
2018-08-10 08:50:18 +0900

    p305 ポストデータ $_POST
    ポストデータを取得する

---
+++ chap8/post1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+</head>
+<title>POSTデータ</title>
+    <body>
+    <form action="" method="post2.php">
+        <label for="name">名前</label>
+        <input id="name" type="text" name="name" size="15">
+        <input type="submit" value="送信">
+    </form>
+    </body>
+</html>
