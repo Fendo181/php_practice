51e3cf30d3c965dc883782ce557b2a7691f6f463
Fendo181 <fendo181git@gmail.com>
2018-08-21 08:43:32 +0900

    p312 クエリ情報を取得する

---
+++ chap8/get1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+</head>
+<title>クエリ情報</title>
+    <body>
+    <!-- action属性にはpost先のファイルを指定する   -->
+    <form method="get" action="get2.php">
+        <label for="name">名前</label>
+        <input id="name" type="text" name="name" size="15">
+        <input type="submit" value="送信">
+    </form>
+    </body>
+</html>
