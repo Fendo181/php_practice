1daa95bc28cca548dca9dabc075e187f7a6134f2
Fendo181 <fendo181git@gmail.com>
2018-08-20 09:31:01 +0900

    p307 fix
    actionが間違えてmethdoに入っていたので修正した

--- chap8/post1.php
+++ chap8/post1.php
@@ -5,7 +5,8 @@
 </head>
 <title>POSTデータ</title>
     <body>
-    <form action="" method="post2.php">
+    <!-- action属性にはpost先のファイルを指定する   -->
+    <form method="post" action="post2.php">
         <label for="name">名前</label>
         <input id="name" type="text" name="name" size="15">
         <input type="submit" value="送信">
