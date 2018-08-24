889133cc7fae1e2d2e9d3249d64aed7f3ea38be5
Fendo181 <fendo181git@gmail.com>
2018-08-24 09:34:47 +0900

    p328 強制的に認証ダイアログを表示させる
    強制的にBasic認証を行う
    
    header('HTTP/1.1 401 Unauthorized');
    header('WWW-Authenticate: Basic realm="SelfPHP"');

---
+++ chap8/Header/authorize.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/08/24
+ * Time: 9:25
+ */
+
+if(!isset($_SERVER['PHP_AUTH_USER'])) {
+    // 認証ユーザが未設定の場合は未認証ステータスを発行
+    header('HTTP/1.1 401 Unauthorized');
+    header('WWW-Authenticate: Basic realm="SelfPHP"');
+    print 'この画面のアクセスは拒否されました';
+    die();
+} else {
+    // 認証に成功した場合に応じてメッセージを送る
+    if(($_SERVER['PHP_AUTH_USER']) === 'admin' && $_SERVER['PHP_AUTH_PW'] === 'admin_pass') {
+        print '正しく認証されました！';
+    } else {
+        print 'ユーザ名、パスワードが間違っています！';
+    }
+}
