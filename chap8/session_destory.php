6479f68ade8163a9ae13e1c1555cfba70db7a1c3
Fendo181 <fendo181git@gmail.com>
2018-08-31 10:22:26 +0900

    p342 セッションを明示的に破棄する

---
+++ chap8/session_destory.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+$_SESSION = [];
+
+
+//クッキーが存在する場合に破棄する
+if(isset($_COOKIE[session_name()])) {
+    $cparam = session_get_cookie_params();
+    setcookie(session_name(), '',time()-3600,
+        $cparam['path'],$cparam['domain'],
+        $cparam['secure'],$cparam['httponly']
+    );
+    echo "SESSIONクッキーを破棄しました";
+}
+
+// セッション破棄
+session_destroy();
+echo "セッションを破棄しました。";
+
