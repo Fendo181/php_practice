96e9de688e60866ee6cc1c0525e7c1ad5c8e808e
Fendo181 <fendo181git@gmail.com>
2018-09-04 23:30:42 +0900

    0383 接続スクリプトの外部化
    毎回Databaaseの設定を読み込むのは大変なのでちゃんと外部化させる

---
+++ chap9/DbManager.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/09/04
+ * Time: 23:26
+ */
+
+function getDb() {
+    $dsn = 'mysql:dbname=selfphp;host=127.0.0.1;charset=utf8';
+    $user = 'selfuser';
+    $password = 'selfpass';
+
+    // DBの接続を確立させる
+    $db = new PDO($dsn,$user,$password);
+    return $db;
+}
---
+++ chap9/connection_common.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/09/04
+ * Time: 23:27
+ */
+
+require_once 'DbManager.php';
+$db = getDb();
+var_dump($db);
+
+//object(PDO)#1 (0) {
+//}
