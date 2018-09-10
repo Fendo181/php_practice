b6cde8c63b4b82ecef3ef1ac54431da4bf9048b4
Fendo181 <fendo181git@gmail.com>
2018-09-10 23:56:10 +0900

    p387 PDO::ATTR_CASE

---
+++ chap9/error/attr_case.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: endu
+ * Date: 2018/09/10
+ * Time: 23:50
+ */
+
+require_once '../DbManager.php';
+
+$db = getDb();
+$db->setAttribute(PDO::ATTR_CASE, PDO::CASE_UPPER);
+print_r($db->query('SELECT * FROM')->fetchAll(PDO::FETCH_ASSOC));
+
+
