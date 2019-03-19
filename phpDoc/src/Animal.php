d059d7438d392bf0fd8d360ae6628c41211040ef
Fendo181 <fendo181git@gmail.com>
2019-03-19 10:37:02 +0900

    簡単なクラスを作成した

---
+++ phpDoc/src/Animal.php
@@ -0,0 +1,49 @@
+<?php
+
+class Dog
+{
+    protected $name;
+    protected $voice;
+
+
+    public function __construct($name, $voice = 'わんわん')
+    {
+        $this->name = $name;
+        $this->voice = $voice;
+
+    }
+
+    public function bark()
+    {
+      echo $this->voice;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        if(isset($name)){
+            return 'please my name';
+        }
+
+        $this->name = $name;
+    }
+}
+
+
+$dog = new Dog('ポコちゃん');
+$dog->bark();
+$name = $dog->getName();
+echo  $name;
+
+
+$fuwa = new Dog('ふわちゃん','こんにちは!');
+$fuwa->bark();
+$fuwa->getName();
+$name = $fuwa->getName();
+echo  $name;
+
+
