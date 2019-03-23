c66845881fcbd6e6a8ce8a4122f55f4ac6d50e18
Fendo181 <fendo181git@gmail.com>
2019-03-23 22:35:21 +0900

    リクエスト時にユーザのセッションを管理するSessionStorageクラスとユーザの情報を管理するUserクラスを定義した

--- di/App.php
+++ di/App.php
@@ -1,8 +1,59 @@
 <?php
 
-$_SESSION['language'] = 'fr';
+/**
+ *
+ * セッションを扱う為のクラス
+ *
+ * Class SessionStorage
+ */
+class SessionStorage
+{
+    function __construct($cookieName = 'PHP_SESS_ID_Change')
+    {
+        session_name($cookieName);
+        session_start();
+    }
 
-$user_language = $_SESSION['language'];
+    function set($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
 
+    function get($key)
+    {
+        return $_SESSION[$key];
+    }
 
-var_dump($user_language);
+}
+
+/**
+ *
+ * ユーザが仕様する言語を取得したり、設定したりする
+ *
+ * Class User
+ */
+class User
+{
+    protected $storage;
+
+    function __construct()
+    {
+        $this->storage = new SessionStorage();
+    }
+
+    function setLanguage($language)
+    {
+        $this->storage->set('language', $language);
+    }
+
+    function getLanguage()
+    {
+        return $this->storage->get('language');
+    }
+}
+
+$user = new User();
+$user->setLanguage('jp');
+$user_language = $user->getLanguage();
+
+echo  "使っている言語は{$user_language}です";
