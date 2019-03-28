4d11dfb61ca642a6c8084d9c089401d78428c0d6
Fendo181 <fendo181git@gmail.com>
2019-03-28 21:24:10 +0900

    DIの例を取ったクラスを実際に作成した

--- Laravel_architecture_practice/DI/di_practice.php
+++ Laravel_architecture_practice/DI/di_practice.php
@@ -13,7 +13,7 @@ interface GamePlayerInterface
     public  function play();
 }
 
-interface TwitterClientINterface
+interface TwitterClientInterface
 {
     public function post();
 }
@@ -31,23 +31,23 @@ Class GameSoft implements GameSoftInterface
 
     public function getTitle()
     {
-        return $this->title();
+        return $this->title;
     }
 }
 
 // PS4クラス
 class Ps4 implements GamePlayerInterface
 {
-    private $gameDisc;
+    private $gameSoft;
 
-    public function __construct(GameSoftInterface $gameDisc)
+    public function __construct(GameSoftInterface $gameSoft)
     {
-        $this->gameDisc = $gameDisc;
+        $this->gameSoft = $gameSoft;
     }
 
     public function play()
     {
-        echo "{$this->gameDisc->getTitle()}をプレイしたい！！！";
+        echo "{$this->gameSoft->getTitle()}をプレイしたい！！！";
     }
 }
 
@@ -59,6 +59,43 @@ class TwitterClient implements TwitterClientInterface
     }
 }
 
+
+//クラス作成
+class Endo
+{
+    private $gameSpft;
+    private $ps4;
+    private $twitter_client;
+
+    public function __construct(GameSoftInterface $gameSpft,GamePlayerInterface $ps4, TwitterClientInterface $twitter_client)
+    {
+        $this->gameSpft = $gameSpft;
+        $this->ps4 = $ps4;
+        $this->twitter_client = $twitter_client;
+    }
+
+    public function play()
+    {
+        echo "今日は{$this->gameSpft->getTitle()}を持ってきました！ ";
+        echo "{$this->ps4->play()}";
+    }
+
+    public function tweet()
+    {
+        $this->twitter_client->post();
+    }
+}
+
+
+$gamesoft =new GameSoft('SEKIRO');
+$famikon = new Ps4($gamesoft);
+$twitter_client = new TwitterClient();
+
+$nikonama = new Endo($gamesoft,$famikon,$twitter_client);
+$nikonama->play();
+$nikonama->tweet();
+
+//給付はSEKIROを持ってきました！ SEKIROをプレイしたい！！！めっちゃ面白いなう！%
 
 
 
