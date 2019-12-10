9f529c2b7b4bd16d69f9580b0a557e87b3d311cc
Fendo181 <fendo181git@gmail.com>
2019-12-10 10:05:05 +0900

    addValue、canAdd、addメソッドを作って、値が作られる場合でも制限を設けるようにした

--- PrinciplesSystemDesign/chap1/Quantity.php
+++ PrinciplesSystemDesign/chap1/Quantity.php
@@ -20,7 +20,37 @@ class Quantity
 
         $this->value = $value;
     }
+
+    private function addValue($other) {
+        $sum = $this->value + $other;
+        return $sum;
+    }
+
+
+    public function canAdd(int $other) :bool {
+        $added = self::addValue($other);
+        return $added <= self::MAX;
+    }
+
+    public function add(int $other) {
+
+        try{
+            if(!$this->canAdd($other)) {
+                throw new Exception('合計が100を超えています');
+            }
+            $added = $this->addValue($other);
+            // addすると新しいオブジェクトを呼び出す
+            return new Quantity($added);
+
+        }catch (Exception $e){
+            echo '捕捉した例外: ',  $e->getMessage(), "\n";
+        }
+    }
 }
 
-$quanty = new Quantity('200');
+// これで一回新しいオブジェクトを生成する
+$quanty = new Quantity(10);
 echo $quanty->value;
+
+// 新しいオブジェクトを生成する
+$quanty->add(100);
