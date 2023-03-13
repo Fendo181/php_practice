b17c66d8f9d948d3716b9b71e0e50d63654d6d4a
Fendo181 <fendo181git@gmail.com>
2023-03-14 00:02:12 +0900

    array_push、array_popも追加

--- review/buidlin_php_grammer/arrray_push.php
+++ review/buidlin_php_grammer/arrray_push.php
@@ -11,5 +11,12 @@ print_r($scores);
 // 逆に末尾に追加するには array_push()
 array_push($scores, 60, 70);
 print_r($scores);
+$scores[] = 80;
+
 // 先頭から要素を削除するには array_shift()
+array_shift($scores);
+print_r($scores); // 10が消える
+
 // 末尾から要素を削除するには array_pop()
+array_pop($scores);
+print_r($scores); // 80が消える
