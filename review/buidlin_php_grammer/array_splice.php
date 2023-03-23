ffdc03ba550b9b0ca5633dea30266fd864883827
Fendo181 <fendo181git@gmail.com>
2023-03-24 07:54:49 +0900

    12 array_splice()で要素を削除､置換しよう

---
+++ review/buidlin_php_grammer/array_splice.php
@@ -0,0 +1,17 @@
+<?php
+// array_splice() 途中の要素を削除して詰める
+// array_sliceと違って、array_spliceは元配置を直接変更します。
+$scores1 = [30, 40, 50, 60, 70, 80];
+array_splice($scores1,2,3);
+print_r($scores);
+
+// array_splice(配列、位置、個数、要素)で指定すると、削除した位置に要素を入れる事ができる
+$scores2 = [30, 40, 50, 60, 70, 80];
+array_splice($scores2,2,3,100);
+print_r($scores2);
+
+
+// array_splice(配列、位置、0、要素)で指定すると、指定した位置に要素を入れる事ができる
+$scores3 = [30, 40, 50, 60, 70, 80];
+array_splice($scores3,2,3,100);
+print_r($scores3);
