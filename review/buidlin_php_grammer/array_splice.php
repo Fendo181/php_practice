e4c72f288e9530e7e08ed8b5be7f771703b6d67f
Fendo181 <fendo181git@gmail.com>
2023-05-07 17:18:43 +0900

    fix

--- review/buidlin_php_grammer/array_splice.php
+++ review/buidlin_php_grammer/array_splice.php
@@ -3,7 +3,7 @@
 // array_sliceと違って、array_spliceは元配置を直接変更します。
 $scores1 = [30, 40, 50, 60, 70, 80];
 array_splice($scores1,2,3);
-print_r($scores);
+print_r($scores1);
 
 // array_splice(配列、位置、個数、要素)で指定すると、削除した位置に要素を入れる事ができる
 $scores2 = [30, 40, 50, 60, 70, 80];
@@ -12,6 +12,6 @@ print_r($scores2);
 
 
 // array_splice(配列、位置、0、要素)で指定すると、指定した位置に要素を入れる事ができる
-$scores3 = [30, 40, 50, 60, 70, 80];
-array_splice($scores3,2,3,100);
-print_r($scores3);
+$scores3 = [30, 40, 50];
+array_splice($scores3,2,0,100);
+print_r($scores3); // 20,40,100,50
