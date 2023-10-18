eeb7a4c0a539703ea9bbf2a8f10566eeaec70d1a
Fendo181 <fendo181git@gmail.com>
2023-10-19 00:10:58 +0900

    13 子クラスで独自の実装をしていこう

--- review/oop_php_grammer/class_extends.php
+++ review/oop_php_grammer/class_extends.php
@@ -19,14 +19,26 @@ class Post // 親クラス、スーパークラス
 // extends Post とするだけで Post クラスのプロパティやメソッドを使うことができます。
 class SponsoredPost extends Post // 子クラス
 {
+    private $sponser;
 
+    public function __construct($text, $sponser)
+    {
+      parent::__construct($text);
+      $this->sponser = $sponser;
+    }
+
+    public function showSponsor()
+    {
+      printf('%s' . PHP_EOL, $this->sponser);
+    }
 }
 
 $posts = [];
 $posts[0] = new Post('hello');
 $posts[1] = new Post('hello again');
-$posts[2] = new SponsoredPost('Hello Hello');
+$posts[2] = new SponsoredPost('Hello Hello','Bob');
 
 $posts[0]->show(); // hello
 $posts[1]->show(); // hello again
 $posts[2]->show(); // Hello Hello
+$posts[2]->showSponsor(); // Bob
