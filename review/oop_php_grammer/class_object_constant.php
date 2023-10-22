a0d7192fe96b36f12efa71499eac0b1f9d1c1fc3
Fendo181 <fendo181git@gmail.com>
2023-10-22 16:03:33 +0900

    fix: サンプルコードを修正した

--- review/oop_php_grammer/class_object_constant.php
+++ review/oop_php_grammer/class_object_constant.php
@@ -1,12 +1,11 @@
 <?php
-
 class Post
 {
   private $text;
   private static $count = 0;
   // クラスに紐付いた定数を「オブジェクト定数」と呼びます
-  // private const VERSION = 0.1;
-  public const VERSION = 0.2;
+  private const VERSION1 = 0.1;
+  public const VERSION2 = 0.2;
 
   public function __construct($text)
   {
@@ -22,17 +21,18 @@ class Post
   public static function showInfo()
   {
     printf('Count: %d' . PHP_EOL, self::$count);
-    printf('Version: %.1f' . PHP_EOL, self::VERSION); // 0.2
+    printf('Version: %.1f' . PHP_EOL, self::VERSION2); // 0.2
   }
 }
 
-$posts = [];
-$posts[0] = new Post('hello');
-$posts[1] = new Post('hello again');
+// $posts = [];
+// $posts[0] = new Post('hello');
+// $posts[1] = new Post('hello again');
 
-$posts[0]->show();
-$posts[1]->show();
+// $posts[0]->show();
+// $posts[1]->show();
 
 Post::showInfo();
 
-echo Post::VERSION . PHP_EOL;
+// echo Post::VERSION1 . PHP_EOL; // Uncaught Error: Cannot access private constant Post::VERSION1
+echo Post::VERSION2 . PHP_EOL;
