0902ada4d46d45cc6693bd28bd4d2219bacb046c
Fendo181 <fendo181git@gmail.com>
2023-10-30 09:46:44 +0900

    16 抽象クラスを使ってみよう

---
+++ review/oop_php_grammer/class_abstract.php
@@ -0,0 +1,56 @@
+<?php
+
+// 抽象クラスはそれ自体からはインスタンスを作ることができない
+// 継承を前提としたクラスになります
+abstract class BasePost
+{
+  // 抽象メソッド
+  // BasePost クラスを Post と SponsoredPost クラスで継承してあげれば
+  // 子クラスのほうで show() メソッドの定義を強制することができる。
+  abstract public function show();
+}
+
+class Post extends BasePost
+{
+  protected $text;
+
+  public function __construct($text)
+  {
+    $this->text = $text;
+  }
+
+  public function show()
+  {
+    printf('%s' . PHP_EOL, $this->text);
+  }
+}
+
+class SponsoredPost extends Post
+{
+  private $sponsor;
+
+  public function __construct($text, $sponsor)
+  {
+    parent::__construct($text);
+    $this->sponsor = $sponsor;
+  }
+
+  public function show()
+  {
+    printf('%s by %s' . PHP_EOL, $this->text, $this->sponsor);
+  }
+}
+
+$posts = [];
+$posts[0] = new Post('hello');
+$posts[1] = new Post('hello again');
+$posts[2] = new SponsoredPost('hello hello', 'dotinstall');
+
+function processPost(Post $post)
+{
+  $post->show();
+}
+
+foreach ($posts as $post) {
+  processPost($post);
+}
